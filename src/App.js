5aa44ea09172c9136890aea826c411162157c680
shaydeecoder <shaydeecoder@gmail.com>
2021-08-31 15:59:13 +0100

    added on functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// Styles
+import "./App.css";
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    if (
+      (operators.includes(value) && calc === "") ||
+      (operators.includes(value) && operators.includes(calc.slice(-1)))
+    )
+      return;
+
+    setCalc(calc + value);
+
+    if (!operators.includes(value)) setResult(eval(calc + value).toString());
+  };
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => updateCalc(`${i}`)}>
+          {i}
+        </button>
+      );
+    }
+
+    return digits;
+  };
+
+  const calculate = () => setCalc(eval(calc).toString());
+
+  const deleteLast = () => {
+    if (!calc) return;
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+
+    if (calc.length === 1) setResult("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="calculator__display">
+          <span>{result ? `(${result})` : ""}</span> {calc || "0"}
+        </div>
+
+        <div className="calculator__operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+
+          <button onClick={deleteLast}>DEL</button>
+        </div>
+
+        <div className="calculator__digits">
+          {createDigits()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
